8ef0420050653ea5dcf1039950bf7acb23b0a7b5
Mykyta Shvets <mdy678@usask.ca>
2023-01-13 16:48:40 -0600

    Init commit

---
+++ Assets/Scripts/LambdaExpr.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambdaExpr : MonoBehaviour
+{
+    // Children of this object are PieceScript objects
+    // which are clauses of a lambda expression
+
+    public PieceScript openingBracket;
+    public PieceScript closingBracket;
+
+    public string variableName = "x";
+
+    public GameObject toReplaceWith;
+
+    public bool testReplace = false;
+
+    public bool testUnpack = false;
+
+    public void ReplacePiecesWith(GameObject obj) {
+        // Replace all pieces with variableName with object
+        List<GameObject> toDestroy = new List<GameObject>();
+        List<GameObject> toInstantiate = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++) {
+            var child = transform.GetChild(i);
+            var piece = child.GetComponent<PieceScript>();
+            if (piece != null) {
+                if (piece.variableName == variableName && piece.isVariable) {
+                    toDestroy.Add(child.gameObject);
+                    toInstantiate.Add(obj);
+                }
+            }
+            var lambda = child.GetComponent<LambdaExpr>();
+            if (lambda != null) {
+                lambda.ReplacePiecesWith(obj);
+            }
+        }
+        for (int i = 0; i < toDestroy.Count; i++) {
+            // instantiate at the same position as destroyed object
+            var pos = toDestroy[i].transform.position;
+            var newObj = Instantiate(toInstantiate[i], pos, Quaternion.identity, transform);
+            // For all children in new object, set their position to pos + j
+            // TODO: Make it recursive
+            for (int j = 0; j < newObj.transform.childCount; j++) {
+                var child = newObj.transform.GetChild(j);
+                child.transform.position = pos + child.transform.localPosition*0.01f;
+            }
+            Destroy(toDestroy[i]);
+        }
+    }
+
+    public void Unpack() {
+        // Removes the starting and ending bracket from this expression
+        openingBracket.gameObject.SetActive(false);
+        closingBracket.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (testReplace) {
+            testReplace = false;
+            ReplacePiecesWith(toReplaceWith);
+        }
+        if (testUnpack) {
+            testUnpack = false;
+            Unpack();
+        }
+    }
+}
---
+++ Assets/Scripts/PieceScript.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PieceScript : MonoBehaviour
+{
+
+    public bool openingBracket = false;
+    public bool closingBracket = false;
+
+    public bool isVariable = false;
+    public string variableName = "x";
+
+
+    public bool inflate = true;
+
+    public float inflateSpeed = 1;
+
+    float inflateProgress = 0;
+
+    public LambdaExpr GetParentExpr() {
+        var parent = transform.parent;
+        while (parent != null) {
+            var lambda = parent.GetComponent<LambdaExpr>();
+            if (lambda != null) {
+                return lambda;
+            }
+            parent = parent.parent;
+        }
+        return null;
+    }
+
+    public GameObject GetParentOrSelf() {
+        var parent = GetParentExpr();
+        if (parent == null) {
+            return gameObject;
+        }
+        return parent.gameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inflateProgress = 0;
+        if (isVariable)
+            GetComponentInChildren<TextMeshPro>().text = variableName;
+        else if (openingBracket)
+            GetComponentInChildren<TextMeshPro>().text = variableName + ".(";
+        else if (closingBracket)
+            GetComponentInChildren<TextMeshPro>().text = ")";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inflate) {
+            transform.localScale = new Vector3(inflateProgress, 1, 1);
+            if (inflateProgress < 1) {
+                inflateProgress += Time.deltaTime * inflateSpeed;
+            } else {
+                inflateProgress = 1;
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        var otherPiece = other.gameObject.GetComponent<PieceScript>();
+        if (otherPiece != null) {
+            if (otherPiece.inflateProgress < 1) return;
+            if (this.inflateProgress < 1) return;
+            if (closingBracket && otherPiece.transform.position.x > transform.position.x) {   
+                Debug.Log("Closing bracket hit");
+                var fullOtherPiece = otherPiece.openingBracket ? 
+                    otherPiece.GetParentOrSelf() : otherPiece.gameObject;
+                var myExpr = GetParentExpr();
+                if (myExpr != null && myExpr.gameObject != fullOtherPiece) {
+                    myExpr.ReplacePiecesWith(fullOtherPiece.gameObject);
+                    myExpr.Unpack();
+                    fullOtherPiece.SetActive(false);
+                }
+            }
+        }
+    }
+
+}
